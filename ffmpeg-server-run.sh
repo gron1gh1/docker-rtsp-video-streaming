7af448d71e251139404911667d07caa72ed7b91a
gron1gh1 <gron1gh1@gmail.com>
2020-09-26 03:06:31 +0000

    (Modify) Container in video filename extension remove

--- ffmpeg-server-run.sh
+++ ffmpeg-server-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 dockerize -wait tcp://server:8554 -timeout 10s
-ffmpeg -re -stream_loop -1 -i video.mp4 -c copy -f rtsp -rtsp_transport tcp rtsp://server:8554/test
+ffmpeg -re -stream_loop -1 -i video -c copy -f rtsp -rtsp_transport tcp rtsp://server:8554/test
 
